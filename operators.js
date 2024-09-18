890b57f0f0bc23dcbe051973503b055a4ea6fe63
shifas <sshifas333@gmail.com>
2024-09-18 11:29:07 +0530

    operator,data

---
+++ operators.js
@@ -0,0 +1,38 @@
+let a=10
+let b='5'
+
+//arithematic 
+    // alert(a+b)
+    // alert(a-b)
+    // alert(a/b)
+    // alert(a*b)
+    // alert(a%b)
+    // a--
+    // a++
+    // alert(a)
+
+
+// assignment
+    // a+=10
+    // a-=10
+    // a/=10
+    // a*=10
+    // a%=10
+    //alert(a)
+
+
+// comparission
+    // alert(a<b)
+    // alert(a>b)
+    // alert(a<=b)
+    // alert(a>=b)
+    // alert(a==b)
+    // alert(a!=b)
+    // alert(a===b)
+    // alert(a!==b)
+
+
+// logical
+    alert(a==b && a>b )
+    alert(a==b || a>b )
+    alert(! a==b)
--- script.js
+++ script.js
@@ -1,10 +1,44 @@
 // a= "Hello"
 
+
+
 // var a=10
 // a=20
 
 // let a=10
 
-const a=15
+// const a=15
 
-alert(a)
+
+// datatypes
+// 1.number
+    // let a=10
+    // let a=1.1
+
+
+// 2.string
+    // let a=23456787
+
+
+// 3.boolean
+    // let a=true
+    // let a=false
+
+
+// 4.undfined
+    // let a
+
+
+// 5.null
+    // let a=null
+
+
+// 6.object
+    // array(list)
+        // let a=[1,2,3]
+    // object(dictionary)
+        // let a={name:'abc'}
+
+// 7.bigint
+
+alert(typeof a)
