4874732aece75229c0b7ab611529b330439ce573
alan2325 <alalkumar06@gmail.com>
2024-09-21 11:28:01 +0530

    form

---
+++ form_task/form.js
@@ -0,0 +1,7 @@
+function valid(){
+    document.getElementById("no").innerHTML
+    name=document.getElementById("name").value
+    no=document.getElementById("no").value
+    email=document.getElementById("email").value
+    pin=document.getElementById("pin").value
+}
--- string_method/methods.js
+++ string_method/methods.js
@@ -1,16 +1,16 @@
 a="Hello welcome to 5678 a55ll"
 b="         ITS A DEMO          "
-// console.log(a);
-// console.log(a[3]);
-// console.log(a.length);
-// console.log(a.toUpperCase());
-// console.log(b.toLowerCase());
-// console.log(b.trim(),'helo');
-// console.log(b.trimStart(),'helo');
-// console.log(b.trimEnd(),'helo');
-// console.log(a.charAt(0));
-// console.log(a.charCodeAt(0));
-// console.log(a.slice(2,4));
+console.log(a);
+console.log(a[3]);
+console.log(a.length);
+console.log(a.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.trim(),'helo');
+console.log(b.trimStart(),'helo');
+console.log(b.trimEnd(),'helo');
+console.log(a.charAt(0));
+console.log(a.charCodeAt(0));
+console.log(a.slice(2,4));
 
 
 console.log(a.match("Hello"))
@@ -18,5 +18,5 @@ console.log(a.match("[a-z][0-9]"))
 console.log(a.match("[a-z0-9]"))
 
 
-// console.log(a.search("Hello"));
-// console.log(a.search("hloo"));
+console.log(a.search("Hello"));
+console.log(a.search("hloo"));
