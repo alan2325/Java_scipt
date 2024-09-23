6f89316d4634c876938e13888d0f79a203367160
alan2325 <alalkumar06@gmail.com>
2024-09-23 10:21:44 +0530

    login form

--- validation/script.js
+++ validation/script.js
@@ -11,13 +11,15 @@ function valid(){
     if(phno.length!=10){
         document.getElementById("num_error").innerHTML="invalid number"
     }
-    
+    else{
+        if(! phno.match('[6-9].{9}')){
+            document.getElementById("num_error").innerHTML="invalid number"
+        }
+    }
     if(pin.length!=6){
         document.getElementById("pin_error").innerHTML="invalid pin"
     }
-    if(email.match("@gmail.com")){
-    }
-    else{
+    if(! email.match("@gmail.com$")){
         document.getElementById("email_error").innerHTML="invalid email"
     }
 
