c39d16ee2ec2ac55f4210a06095678ac0f57dca2
alan2325 <alalkumar06@gmail.com>
2024-09-25 11:31:17 +0530

    form

--- task1/task1.js
+++ task1/task1.js
@@ -6,6 +6,46 @@ let d = [
     {name: 'nu', age: 20, place: 'tkm'}
 ]
 
-d.forEach(Element => {
-    console.log(element.age);
+
+function display(){
+    const t_var=document.querySelector('.tbl tbody')
+    t_var.innerHTML=''
+    d.forEach(element => {
+        const t_row=document.createElement('tr')
+
+        const t_name=document.createElement('td')
+        t_name.textContent=element.name
+        t_row.appendChild(t_name)
+
+
+        const t_age=document.createElement('td')
+        t_age.textContent=element.age
+        t_row.appendChild(t_age)
+
+
+        const t_place=document.createElement('td')
+        t_place.textContent=element.place
+        t_row.appendChild(t_place)
+
+        
+
+        t_var.appendChild(t_row)
+    });
+
+}
+
+
+document.getElementById('submit_form').addEventListener('submit',function(event){
+    event.preventDefault()
+    const name=document.getElementById('name').value
+    const age=parseInt(document.getElementById('age').value)
+    const place=document.getElementById('place').value
+    d.push({name:name,age:age,place:place})
+    document.getElementById('name').value=''
+    document.getElementById('age').value=''
+    document.getElementById('place').value=''
+
+    display()
 })
+
+display()
