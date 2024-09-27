d66376a229a963d4f1247a8650420c18fbb36b8f
alan2325 <alalkumar06@gmail.com>
2024-09-27 11:25:24 +0530

    methods

--- spread/script.js
+++ spread/script.js
@@ -47,3 +47,14 @@ function display(){
     console.log('display');
 }
 const a=setInterval(display,2000)
+
+
+let count=10
+let c=setInterval(function(){
+    console.log(count);
+    count--
+    if(count==0){
+        clearInterval(c)
+        console.log('completed');
+    }
+},1000)
