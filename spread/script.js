71a018e87942ba8594e75375428c8114b919fd37
alan2325 <alalkumar06@gmail.com>
2024-09-27 11:13:01 +0530

    methods

--- spread/script.js
+++ spread/script.js
@@ -1,8 +1,43 @@
-l=[1,2,3,4]
-l1=[...l,5,6]
-console.log(l1);
+// spread
+// l=[1,2,3,4]
+// l1=[...l,5,6]
+// console.log(l1);
 
 
-d={name:'abc',age:24}
-d1={...d,place:'ekm',name:'abcd',age:20}
-console.log(d1);
+// d={name:'abc',age:24}
+// d1={...d,place:'ekm',name:'abcd',age:20}
+// console.log(d1);
+
+// structuring
+
+// l=[1,2,3,4]
+// const[a,...b]=l
+// console.log(a);
+// console.log('b=',b);
+
+// d={name:'abc',age:24,place:'ekm'}
+
+// const{name,...d1}=d
+// console.log(name);
+// console.log(d1);
+
+
+// calback
+
+console.log('start');
+function display(callback){
+    console.log('welcome');
+    setTimeout(function(){
+        console.log('inside fun');
+        callback()
+
+    },3000)
+}
+
+function display1(){
+    console.log('dis');
+}
+
+console.log('before');
+display(display1)
+console.log('end');
