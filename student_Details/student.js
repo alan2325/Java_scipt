77a2b7b0538ddabbf83571b22eca333e6185dadb
alan2325 <alalkumar06@gmail.com>
2024-10-01 11:06:58 +0530

    form

--- student_Details/student.js
+++ student_Details/student.js
@@ -1,16 +1,18 @@
 let s=[
-    {id:1, name: 'anu', age: 20, email: 'anu@gmail.com', cource:'eng', date:08/12/2016},
-    {id:2, name: 'sanu', age: 21, email: 'sanu@gmail.com', cource:'mal', date:08/12/2016},
+    {id:1, name: 'anu', age: 20, email: 'anu@gmail.com', cource:'eng', date:'08/12/2016'},
+    {id:2, name: 'sanu', age: 21, email: 'sanu@gmail.com', cource:'mal', date:'08/12/2016'},
 ]
 
-function display(){
+function display()
+{ 
     const t_var=document.querySelector('.tbl tbody')
     t_var.innerHTML=''
-    d.forEach(element => {
+    s.forEach(element => {
+
         const t_row=document.createElement('tr')
 
         const t_id=document.createElement('td')
-        t_name.textContent=element.id
+        t_id.textContent=element.id
         t_row.appendChild(t_id)
 
         const t_name=document.createElement('td')
@@ -18,28 +20,41 @@ function display(){
         t_row.appendChild(t_name)
 
         const t_age=document.createElement('td')
-        t_name.textContent=element.age
+        t_age.textContent=element.age
         t_row.appendChild(t_age)
 
         const t_email=document.createElement('td')
-        t_name.textContent=element.email
+        t_email.textContent=element.email
         t_row.appendChild(t_email)
 
         const t_cource=document.createElement('td')
-        t_name.textContent=element.cource
+        t_cource.textContent=element.cource
         t_row.appendChild(t_cource)
 
         const t_date=document.createElement('td')
-        t_name.textContent=element.date
+        t_date.textContent=element.date
         t_row.appendChild(t_date)
 
+        const edit_td=document.createElement("td")
+        const edit_btn=document.createElement("button")
+        edit_btn.textContent="Edit"
+        edit_btn.onclick=function () {
+            submit_form(element.id)
+        }
+        edit_td.appendChild(edit_btn)
+        t_row.appendChild(edit_td)
+
+        t_var.appendChild(t_row)
+    });
+}
+
+    
+function submit_form(){
+    
+}
 
 
-        //edit
-
-
-
-
+    // Add
 
         document.getElementById('submit_form').addEventListener('submit',function(event){
             event.preventDefault()
@@ -49,7 +64,7 @@ function display(){
             const email=document.getElementById('email').value
             const cource=document.getElementById('cource').value
             const date=document.getElementById('date').value
-            s.push({id:id,name:name,age:age,email:email,cource:cource,date:date,})
+            s.push({id:id,name:name,age:age,email:email,cource:cource,date:date})
             document.getElementById('id').value=''
             document.getElementById('name').value=''
             document.getElementById('age').value=''
@@ -57,6 +72,52 @@ function display(){
             document.getElementById('cource').value=''
             document.getElementById('date').value=''
 
-        t_var.appendChild(t_row)
-    });
-}
+            display()
+            // console.log('hello');
+        });
+            
+
+     
+    // Edit display
+    
+    let edit_id=""
+    function edit_form(id) {
+        console.log("helo");
+        document.getElementById("edit_form").style.display="grid"
+        document.getElementById("submit_form").style.display="none"
+        const edit_details=s.find(user=>user.id==id)
+        document.getElementById("e_id").value=edit_details.id
+        document.getElementById("e_name").value=edit_details.name
+        document.getElementById("e_age").value=edit_details.age
+        document.getElementById("e_email").value=edit_details.email
+        document.getElementById("e_course").value=edit_details.course
+        document.getElementById("e_date").value=edit_details.date
+        edit_id=id
+    }
+    
+    // Edit
+ 
+document.getElementById("edit_form").addEventListener("submit",function (event) {
+    event.preventDefault()
+    // const e_id=document.getElementById("e_id").value
+    const e_name=document.getElementById("e_name").value
+    const e_age=document.getElementById("e_age").value
+    const e_email=document.getElementById("e_email").value
+    const e_course=document.getElementById("e_course").value
+    const e_date=document.getElementById("e_date").value
+    console.log('hello')
+    user=user.map(user=>{
+        if(user.name==edit_id){
+            return {...user,id:e_id,name:e_name,age:e_age,email:e_email,course:e_course,date:e_date}
+        }
+        return user
+    })
+
+    document.getElementById("edit_form").style.display="none"
+    document.getElementById("submit_form").style.display="grid"
+    display()
+
+})
+
+
+display()
